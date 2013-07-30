fc318058a69765a33ea8a59a6c620aa8be03dcbe
Bim Overbohm <bim.overbohm@googlemail.com>
2013-07-30 21:26:55 +0200

    Add Linux test script

---
+++ run_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./res2h ./test ./results -s -v -h ./results/resources.h -u ./results/resources.cpp
+./res2h ./test ./results/data.bin -b -s -v
+./res2hdump ./results/data.bin ./results -f -v
