d8ffcba544ee5fd125f5cf6331e6a905cbe0265d
Bim <bim.overbohm@googlemail.com>
2013-07-30 20:46:39 +0200

    Update tests to dump binary archive

--- run_test.bat
+++ run_test.bat
@@ -1,2 +1,3 @@
-.\Debug\res2h.exe .\test .\results -s -v -h .\results\resources.h -u .\results\resources.cpp
-.\Debug\res2h.exe .\test .\results\data.bin -b -s -v
+.\Release\res2h.exe .\test .\results -s -v -h .\results\resources.h -u .\results\resources.cpp
+.\Release\res2h.exe .\test .\results\data.bin -b -s -v
+.\Release\res2hdump.exe .\results\data.bin .\results -f -v
