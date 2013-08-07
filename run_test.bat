ee0bff6e224d13968ceaaa88a674d3d7ca138121
Bim <bim.overbohm@googlemail.com>
2013-08-07 23:07:41 +0200

    Add some breaks to test script console output

--- run_test.bat
+++ run_test.bat
@@ -1,5 +1,9 @@
 @echo off
+echo ---------------------------------------------------------------------
 mkdir results
+echo ---------------------------------------------------------------------
 .\Release\res2h.exe .\test .\results -s -v -h .\results\resources.h -u .\results\resources.cpp
+echo ---------------------------------------------------------------------
 .\Release\res2h.exe .\test .\results\data.bin -b -s -v
+echo ---------------------------------------------------------------------
 .\Release\res2hdump.exe .\results\data.bin .\results -f -v
--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+printf "---------------------------------------------------------------------\n"
 mkdir results
+printf "---------------------------------------------------------------------\n"
 ./res2h ./test ./results -s -v -h ./results/resources.h -u ./results/resources.cpp
+printf "---------------------------------------------------------------------\n"
 ./res2h ./test ./results/data.bin -b -s -v
+printf "---------------------------------------------------------------------\n"
 ./res2hdump ./results/data.bin ./results -f -v
